9ebcb284c546a5c56c8e0e4afc4b5b52234e6588
Ahmed Elghareeb <ahmed.elghareeb1166@gmail.com>
2024-04-05 15:11:54 +0200

    draw polylines, polygons

--- lib/widgets/custom_google_map.dart
+++ lib/widgets/custom_google_map.dart
@@ -24,7 +24,9 @@ class _GoogleMapViewState extends State<GoogleMapView> {
       ),
       zoom: 16,
     );
-    initMarkers();
+    // initMarkers();
+    // initPolyLines();
+    // initPolygons();
     super.initState();
   }
 
@@ -42,13 +44,16 @@ class _GoogleMapViewState extends State<GoogleMapView> {
     super.dispose();
   }
 
-  Set<Marker> markers = {};
+  // Set<Marker> markers = {};
+  // Set<Polyline> polyLines = {};
+  // Set<Polygon> polygons = {};
 
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
         GoogleMap(
+          zoomControlsEnabled: false,
           //for first camera open
           initialCameraPosition: initialCameraPosition,
           //to change map style
@@ -57,7 +62,11 @@ class _GoogleMapViewState extends State<GoogleMapView> {
             initMapStyle();
           },
           //for add markers
-          markers: markers,
+          // markers: markers,
+          //to draw lines
+          // polylines: polyLines,
+          //to draw shape
+          // polygons: polygons,
           //to change my location
           // onMapCreated: (ctr) {
           //   ctr = googleMapController;
@@ -95,30 +104,83 @@ class _GoogleMapViewState extends State<GoogleMapView> {
     );
   }
 
-  //to add markers
-  void initMarkers() {
-    // var myMarker = Marker(
-    //   markerId: MarkerId(
-    //     "1",
-    //   ),
-    //   position: LatLng(
-    //     31.06341706114919,
-    //     31.40887747116393,
-    //   ),
-    // );
-    var myMarkers = places
-        .map(
-          (placeModel) => Marker(
-            markerId: MarkerId(placeModel.id.toString()),
-            position: placeModel.latLng,
-            infoWindow: InfoWindow(
-              title: placeModel.name,
-            ),
-          ),
-        )
-        .toSet();
-    markers.addAll(myMarkers);
-  }
+  //to draw shape
+  // void initPolygons() {
+  //   Polygon polygon = Polygon(
+  //     polygonId: const PolygonId(
+  //       "1",
+  //     ),
+  //     points: const [
+  //       LatLng(
+  //         31.062582085583188,
+  //         31.409294777621582,
+  //       ),
+  //       LatLng(
+  //         31.06175761871595,
+  //         31.407005790388748,
+  //       ),
+  //       LatLng(
+  //         31.058220238473474,
+  //         31.403176412353922,
+  //       ),
+  //     ],
+  //     fillColor: Colors.black.withOpacity(0.5),
+  //     strokeWidth: 3,
+  //   );
+  //   polygons.add(polygon);
+  // }
+
+//to draw lines
+// void initPolyLines() {
+//   Polyline polyline = const Polyline(
+//     polylineId: PolylineId(
+//       "1",
+//     ),
+//     width: 5,
+//     color: Colors.red,
+//     startCap: Cap.roundCap,
+//     points: [
+//       LatLng(
+//         31.058220238473474,
+//         31.403176412353922,
+//       ),
+//       LatLng(
+//         31.06175761871595,
+//         31.407005790388748,
+//       ),
+//       LatLng(
+//         31.062582085583188,
+//         31.409294777621582,
+//       ),
+//     ]
+//   );
+//   polyLines.add(polyline);
+// }
+
+//to add markers
+// void initMarkers() {
+//   // var myMarker = Marker(
+//   //   markerId: MarkerId(
+//   //     "1",
+//   //   ),
+//   //   position: LatLng(
+//   //     31.06341706114919,
+//   //     31.40887747116393,
+//   //   ),
+//   // );
+//   var myMarkers = places
+//       .map(
+//         (placeModel) => Marker(
+//           markerId: MarkerId(placeModel.id.toString()),
+//           position: placeModel.latLng,
+//           infoWindow: InfoWindow(
+//             title: placeModel.name,
+//           ),
+//         ),
+//       )
+//       .toSet();
+//   markers.addAll(myMarkers);
+// }
 }
 
 //world view --> range zoom = 0 : 3
